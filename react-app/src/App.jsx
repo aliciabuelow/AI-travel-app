1206b609b2fb8890097e303e7edfa119af1886bc
Alicia Buelow <alicia@buelow.com.au>
2026-03-21 19:49:10 +0800

    Restructured files, deleted unnecessary

--- react-app/src/App.jsx
+++
@@ -1,5 +0,0 @@
-export default function App() {
-  return <>
-  <h1>Plan a trip in seconds with AI</h1>
-  </>
-}
---
+++ react-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.css'
+
+export default function App() {
+  return (
+  <>
+  <div className="App">
+    <h1>Plan a trip in seconds</h1>
+    <h2>Generate a detailed travel itinerary</h2>
+  </div>
+  </>
+  )
+}
